d3f5bf1737434fe34a45ab3bef5dd4507f5bb146
anubhavSharma2002 <165506667+anubhavSharma2002@users.noreply.github.com>
2025-06-06 08:31:05 +0530

    Removed remove.db

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,5 +1,13 @@
-from app import db, app
+from flask import Flask
+from models import db
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    print("✅ Database created")
+    print("✅ Database created (if not already)")
